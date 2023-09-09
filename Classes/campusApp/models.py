a90286d13d74ef5526e11036a9af03e5846344bb
Samp1997 <sampiazza2016@gmail.com>
2023-09-09 13:08:34 -0700

    CampusApp changes

--- Classes/campusApp/models.py
+++ Classes/campusApp/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models of University Classes
 class UniversityCampus(models.Model):
-    name = models.CharField(max_length=60, default="", blank=True, null=False)
-    state = models.CharField(max_length=60, default="", blank=True, null=False)
+    title = models.CharField(max_length=60, default="", blank=True, null=False)
+    duration = models.CharField(max_length=60, default="", blank=True, null=False)
     campus_id = models.IntegerField(default="", blank=True, null=False)
 
     # Creates model manager
@@ -12,7 +12,7 @@ class UniversityCampus(models.Model):
 
     # Displays the object output values in the form of a string
     def __str__(self):
-        display_campus = '{0.title}: {0.instructor_name}'
+        display_campus = '{0.name}: {0.state}'
         return display_campus.format(self)
 
     # removes added 's' that Django adds to the model name in the browser display
